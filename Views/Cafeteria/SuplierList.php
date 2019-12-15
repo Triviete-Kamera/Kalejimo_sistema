05ee041ae48eaf3bc3efe27f1b12350b09a04447
evekaz1 <ekazla@gmail.com>
2019-12-15 22:06:16 +0200

    Setup

---
+++ Views/Cafeteria/SuplierList.php
@@ -0,0 +1,9 @@
+<?php
+$content = '
+        <h3>Suplier list</h3>'
+        ;
+
+$navigation = '<li><a href="index.php">Tiekeju salinimas</a></li>
+				<li><a href="index.php">Tiekeju pridejimas</a></li>
+                   ';
+        include 'Template.php';
