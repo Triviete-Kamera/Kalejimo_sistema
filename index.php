3e2dd11591ee83158317a790c1107dfeac243040
Algraud <algraud@gmail.com>
2019-12-16 13:51:27 +0200

    Navigacija iš home

--- index.php
+++ index.php
@@ -4,55 +4,70 @@ session_start();
 
 // Check if the user is already logged in, if yes then redirect him to welcome page
 if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
-	if ($_SESSION["vartotojo_tipas"] == 'valgyklos_darbuotojas') {
-		$title = "Home";
+    $navigation = '<li><a href="SupplyList.php">Maisto atsargos</a></li>
+                <li><a href="CafeteriaRateList.php">Valgyklos įvertinimai</a></li>
+                <li><a href="Schedule.php">Tvarkaraštis</a></li>
+                <li><a href="ZoneList.php">Kalėjimo zonos</a></li>
+                <li><a href="OffenseReport.php">Nusižengimų ataskaita</a></li>
+                <li><a href="PrisonerList.php">Kaliniai</a></li>
+                <li><a href="SupplyStats.php">Išteklių statistika</a></li>
+                   ';
+    if ($_SESSION["vartotojo_tipas"] == NULL) {
+        
+        $content = '
+        <h3>Sveiki, jūs dar neturite pareigų</h3>
+        ';
+
+        $navigation .= '';
+
+
+    }
+	else if ($_SESSION["vartotojo_tipas"] == 'valgyklos_darbuotojas') {
 		$content = '
         <h3>Sveiki, jūs esate virtuves darbuotojas</h3>
         ';
 
-        $navigation = '<li><a href="/Views/Cafeteria/SuplierList.php">Tiekeju sarasas</a></li>
-                <li><a href="index.php">Valgyklos ivertinimu sarasas</a></li>
-                <li><a href="logout.php">Atsijungti</a></li>
+        $navigation .= '<li><a href="SupplierList.php">Tiekeju sarasas</a></li>
                    ';
-        include 'Template.php';
 
 
 	}
-	if ($_SESSION["vartotojo_tipas"] == 'administratorius') {
+    
+	else if ($_SESSION["vartotojo_tipas"] == 'administratorius') {
 	
-    $title = "Home";
 	$content = '
         <h3>Sveiki, jūs esate administratorius</h3>
         ';
-        include 'template.php';
-        $navigation = '
-                <li><a href="logout.php">Atsijungti</a></li>
+        $navigation .= '
+                <li><a href="OffenseAdd.php">Registruoti Nusižengimą</a></li>
+                <li><a href="OffenseAdd.php">Registruoti Nusižengimą</a></li>
                    ';
+        
     }
-    if ($_SESSION["vartotojo_tipas"] == 'priziuretojas') {
+    else if ($_SESSION["vartotojo_tipas"] == 'priziuretojas') {
     
-    $title = "Home";
     $content = '
         <h3>Sveiki, jūs esate prižiūrėtojas</h3>
         ';
-        $navigation = '
-         <li><a href="zonelist.php">Zonu sarasas</a></li>
-                <li><a href="logout.php">Atsijungti</a></li>
+        $navigation .= '
+                <li><a href="ZoneList.php">Zonų sąrašas</a></li>
+                <li><a href="ScheduleCreate.php">Naujas Tvarkaraštis</a></li>
                    ';
-        include 'Template.php';
+        
     }
+    $navigation .= '<li><a href="logout.php">Atsijungti</a></li> ';
 }
 else {
-	$title = "Home";
-$content = '
+    $content = '
        
         <h3>Triviete kamera</h3>
         ';
-$navigation = '<li><a href="login.php">Prisijungti</a></li>
+    $navigation = '<li><a href="login.php">Prisijungti</a></li>
                 <li><a href="registration.php">Registruotis</a></li>
                    ';
 
         
-include 'Template.php';
 }
+$title = "Home";
+include 'Template.php';
 ?>
