a164d2b46677f9a7bc572e85a4288f296eed9e3d
Algraud <algraud@gmail.com>
2019-12-16 23:21:39 +0200

    PrisonerEdit

--- PrisonerEdit.php
+++ PrisonerEdit.php
@@ -10,26 +10,7 @@ $birth_err = $startDate_err = $cell_err = $name_err = $lastname_err = $personalc
 
 if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true && $_SESSION["vartotojo_tipas"] == 'administratorius' && isset($_SESSION['id'])) {
 	$navigation = $cells = '';
-	$cells = $prisonerController->GetCellOptions();
 	if($_SERVER["REQUEST_METHOD"] == "POST"){
-	 
-	    // Validate id
-	    if(empty(trim($_POST["personalcode"]))){
-	        $personalcode_err = "Įveskite asmens kodą.";
-	    } else{
-	        
-	        if($prisonerController->GetPrisoner(trim($_POST["personalcode"])) != null){
-	        	$personalcode_err = "Toks kalinys jau yra.";
-	        }
-	        else{
-	        	$personalcode = trim($_POST["personalcode"]);
-	        }
-	        
-	         
-	        
-	    }
-	    
-
 	    // Validate name
 	    if(empty(trim($_POST["name"]))){
 	        $name_err = "Įveskite varda.";  }
@@ -67,41 +48,50 @@ if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true && $_SESSION["
 	    
 	    // Check input errors before inserting in database
 	    if(empty($personalcode_err) && empty($name_err) && empty($lastname_err )&& empty($birth_err ) && empty($cell_err )){
-	        $prisonerController->AddPrisoner($personalcode, $name, $lastname, $birth,NULL,$startDate, $_SESSION['id'], $cell);
+	        $prisonerController->EditPrisoner(trim($_POST["prisonerId"]),trim($_POST["personalcode"]), $name, $lastname, $birth,NULL,$startDate, $_SESSION['id'], $cell);
 	        header("location: PrisonerList.php");
 	    }
 	}
+	$prisonerid = $_GET['prisoner'];
+	$prisoner = $prisonerController->GetPrisonerId($prisonerid);
+	$cells = $prisonerController->GetCellOptionsID($prisoner->kamera_id);
+	if($id = NULL){
+		header("location: PrisonerList.php");
+	}
+	
 	       
 }else{
 	header("location: index.php");
 }
+echo $prisonerid;
 $content = '
         <h2>Kalinio Registracija</h2>
         <p>Prašome užpildyti duomenis.</p>
         <form action="'.htmlspecialchars($_SERVER["PHP_SELF"]).'" method="post">
+        		<input type="hidden" id="prisonerId" name="prisonerId" value="'.$prisonerid.'">
             <div class="form-group '.((!empty($personalcode_err)) ? "has-error" : "").'">
                 <label>Asmens kodas</label>
-                <input type="number" name="personalcode" class="form-control" value="">
+                <input type="number" name="personalcode" class="form-control" value="'.$prisoner->asmens_kodas.'" readonly>
                 <span class="help-block">'.$personalcode_err.'</span>
             </div>
             <div class="form-group '.((!empty($name_err)) ? "has-error" : "").'">
                 <label>Vardas</label>
-                <input type="text" name="name" class="form-control" value="">
+                <input type="text" name="name" class="form-control" value="'.$prisoner->vardas.'">
                 <span class="help-block">'.$name_err.'</span>
             </div>
             <div class="form-group '.((!empty($lastname_err)) ? "has-error" : "").'">
                 <label>Pavarde</label>
-                <input type="text" name="lastname" class="form-control" value="">
+                <input type="text" name="lastname" class="form-control" value="'.$prisoner->pavarde.'">
                 <span class="help-block">'.$lastname_err.'</span>
             </div>
             <div class="form-group '.((!empty($birth_err)) ? "has-error" : "").'">
                 <label>Gimimo Data</label>
-                <input type="date" name="birth" class="form-control" value="">
+                <input type="date" name="birth" class="form-control" value="'.$prisoner->gimimo_data.'">
                 <span class="help-block">'.$birth_err.'</span>
             </div>
             <div class="form-group '.((!empty($startDate_err)) ? "has-error" : "").'">
                 <label>Įkalinimo Data</label>
-                <input type="date" name="startDate" class="form-control" value="">
+                <input type="date" name="startDate" class="form-control" value="'.$prisoner->ikalinimo_data.'">
                 <span class="help-block">'.$startDate_err.'</span>
             </div>
             <div class="form-group '.((!empty($cell_err)) ? "has-error" : "").'">
