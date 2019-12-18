115b9225c3bfa68c2e507cc5702b4e4ab5bd9905
untuned1337 <52298228+untuned1337@users.noreply.github.com>
2019-12-18 02:03:22 +0200

    Update SuplierList.php

--- SuplierList.php
+++ SuplierList.php
@@ -1,9 +1,39 @@
 <?php
+
 $content = '
-        <h3>Suplier list</h3>'
-        ;
+        <h3>Tiekėjų sąrašas</h3><br />'
+;
 
 $navigation = '<li><a href="index.php">Tiekeju salinimas</a></li>
 				<li><a href="index.php">Tiekeju pridejimas</a></li>
                    ';
-        include 'Template.php';
+#include 'Template.php';
+include 'Model/config.php';
+
+$content.='<table class="center" border="1" cellspacing="0" cellpadding="3"><tr><td><b>Pavadinimas</b></td><td><b>E. paštas</b></td><td><b>Telefonas</b></td><td><b>Adresas</b></td></tr>';
+
+$sql = "SELECT id, pavadinimas, el_pastas, tel_nr, adresas FROM tiekejas ";
+$result = mysqli_query($link, $sql);
+if (!$result || (mysqli_num_rows($result) < 1))
+{echo "Klaida skaitant lentelę tiekejas"; exit;}
+
+while($row = mysqli_fetch_assoc($result))
+{
+    $id = $row['id'];
+    $pavadinimas = $row['pavadinimas'];
+    $el_pastas = $row['el_pastas'];
+    $tel_nr = $row['tel_nr'];
+    $adresas = $row['adresas'];
+
+    $content .= '<tr>';
+    $content .= '<td>'.$pavadinimas.'</td>';
+    $content .= '<td>'.$el_pastas.'</td>';
+    $content .= "<td>".$tel_nr."</td>";
+    $content .= "<td>".$adresas."</td>";
+    $content .= "</tr>";
+
+}
+
+$content .='</table>';
+include 'Template.php';
+?>
